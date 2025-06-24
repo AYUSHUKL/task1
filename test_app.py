43fd548d6f794dd8a31edd3ff11c683cbf09ac10
Ayush Shukla <80712298+AYUSHUKL@users.noreply.github.com>
2025-06-24 22:22:01 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,5 +4,5 @@ def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
-    # This test is intentionally wrong to trigger a failure
-    assert subtract(10, 5) == 4  # Should be 5, but we write 4
+    # Intentional failure
+    assert subtract(10, 5) == 4  # This should fail, correct value is 5
